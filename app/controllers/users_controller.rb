843e15a977e675744d08b7553671b58f6ad718b0
minorusso <gumbomb1@gmail.com>
2022-02-09 23:51:55 +0900

    Add relationship controller & Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,33 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
+
   end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
+
   def user_params
     params.require(:user).permit(:name, :email, :profile, :profile_image)
+  end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_user
+  end
+  
+  def followers
+    user = User.find(params[:id])
+    @users = user.follower_user
   end
   
 end
