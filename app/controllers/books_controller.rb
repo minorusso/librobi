06148ba572cd8a86a254a197317b75dce43c4085
minorusso <gumbomb1@gmail.com>
2022-02-09 23:15:49 +0900

    Update books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,18 @@
 class BooksController < ApplicationController
   def search
     if params[:keyword]
-      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+      @books = RakutenWebService::Books::Book.search(item:[title: params[:keyword]])
+      binding.irb
+      # @params << RakutenWebService::Books::Book.search(author: params[:keyword])
+      # @books = RakutenWebService::Books::Book.search(author: params[:keyword])
+      # if params(keyword)
+      # @books = RakutenWebService::Books::Book.search(keyword: keyword)
+
+    # if params[:keyword]
+    #   @books = RakutenWebService::Books::Book.search(keyword:params[:keyword])
+      # binding.irb
+
+
     end
   end
 end
