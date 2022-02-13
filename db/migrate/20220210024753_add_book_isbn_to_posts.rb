73a18e4e0b550e3efe88e7b2849387628753daec
minorusso <gumbomb1@gmail.com>
2022-02-14 01:07:49 +0900

    Considering

--- db/migrate/20220210024753_add_book_isbn_to_posts.rb
+++ db/migrate/20220210024753_add_book_isbn_to_posts.rb
@@ -1,7 +1,6 @@
 class AddBookIsbnToPosts < ActiveRecord::Migration[6.0]
   def change
-    add_column :posts, :isbn, :bigint
-    add_foreign_key :posts, :books, column: :isbn , primary_key: :isbn
+    add_column :posts, :isbn, :bigint, index: true
   end
 end
 
