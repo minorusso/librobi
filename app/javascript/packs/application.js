f0b528885421865c5b6314ada81482d2667a577f
minorusso <gumbomb1@gmail.com>
2022-02-17 23:30:34 +0900

    FIx bootstrap introduction

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require('jquery')
+// require('../rails_admin.js')
 
-import 'bootstrap';
-import '../stylesheets/application';
+// import 'bootstrap';
+// import '../stylesheets/application';
 
 window.$ = window.jQuery = require('jquery');
 require('packs/raty')
