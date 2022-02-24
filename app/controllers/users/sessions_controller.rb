c99e5a1e70b2c47b3578a2a29d012021ed4f0d50
minorusso <gumbomb1@gmail.com>
2022-02-24 13:49:17 +0900

    Fix Rails admin Bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,12 +4,12 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
   end
   def admin_guest_sign_in
     user = User.admin_guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザー(管理者)としてログインしました。'
+    redirect_to posts_path, notice: 'ゲストユーザー(管理者)としてログインしました。'
   end
   # before_action :configure_sign_in_params, only: [:create]
 
